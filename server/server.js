5010ca72befe8a8e58dfd7475fbef0cda3241527
Felipe Ramos <129113618+fliperamos88@users.noreply.github.com>
2024-04-27 08:11:30 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import { sequelize } from './config/config.js';
 import Admin from './models/Admin.js';
 import Ticket from './models/Ticket.js';
 import Response from './models/Response.js';
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 // await sequelize.sync({ force: true });
